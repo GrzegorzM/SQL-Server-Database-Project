8c188233bbb62a8f66736b433bbc2d6a02ab6de1
Kesiu <kesiu.pro@gmail.com>
2020-07-21 00:58:23 +0200

    Initial Project commit

---
+++ SQL Server Database Project/Scripts/LessonScripts/2_CreatingAlteringDroppingDatabase.sql
@@ -0,0 +1,13 @@
+﻿-- Create database
+create database SampleDB;
+
+-- Altering database
+alter database SampleDB modify Name = SampleDatabase;
+sp_renameDB 'SampleDatabase', 'SampleDB';
+
+-- Cannot drop database currently in use. Forcing to rollback all transactions and close open connections to database.
+alter database SampleDB set single_user with rollback immediate;
+
+-- Drop database
+drop database SampleDB;
+drop database master; -- Error --> cannot drop system databases
