b8aec2ab538014647edc69703ebe7323fa8bb3b4
Kesiu <kesiu.pro@gmail.com>
2020-07-24 06:32:20 +0200

    Alter table and optional procedure parameters

---
+++ SQL Server Database Project/Procedures/spSearchEmployees.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spSearchEmployees]
+	@Name nvarchar(50) = null,
+	@Email nvarchar(50) = null,
+	@Gender nvarchar(50) = null,
+	@Age int = null
+as
+	select * from tblEmployee
+	where (Name = @Name or @Name is null) and (Gender = @Gender or @Gender is null)
