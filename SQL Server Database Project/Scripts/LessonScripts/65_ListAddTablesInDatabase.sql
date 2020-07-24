941dd6e45f0a03e63d01cef86a1547b199a435b5
Kesiu <kesiu.pro@gmail.com>
2020-07-24 02:53:24 +0200

    List all database object

---
+++ SQL Server Database Project/Scripts/LessonScripts/65_ListAddTablesInDatabase.sql
@@ -0,0 +1,10 @@
+﻿select * from sysobjects; -- where xtype = 'P';
+select distinct xtype from sysobjects;
+
+select * from sys.views;
+select * from sys.tables;
+select * from sys.procedures;
+
+select * from INFORMATION_SCHEMA.VIEWS;
+select * from INFORMATION_SCHEMA.tables;
+select * from INFORMATION_SCHEMA.ROUTINES;
