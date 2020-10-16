84962cf20aadd57b0b2ddd399936f37660a93198
Kesiu <kesiu.pro@gmail.com>
2020-10-16 23:43:50 +0200

    Transaction - dirty read

--- SQL Server Database Project/Scripts/LessonScripts/71_TransactionDirtyRead.sql
+++ SQL Server Database Project/Scripts/LessonScripts/71_TransactionDirtyRead.sql
@@ -1,11 +1,23 @@
-﻿--Transaction 1
+﻿--Transaction 1(New Query Window)
 begin transaction
 	update tblInventory set ItemsInStocks = 9 where Id = 1;
-waitfor delay '00:00:15';
+waitfor delay '00:00:15'; --15 sec delay
 rollback transaction;
 
---Transaction 2
-select * from tblInventory where Id = 1; --blocked till first transaction end
+--Transaction 2(Diffrent Query Window)
+select * from tblInventory where Id = 1; --Read commited(default isolation level of transaction) will be blocked till transaction1 gonna end
+--reading uncommited data = dirty data
 
+--change isolation level so we can read dirty data
 set transaction isolation level read uncommitted; --allowing read uncommitted data = dirty read
 select * from tblInventory where Id = 1;
+
+--nolock same effect as changing isolation level
+select * from tblInventory (nolock) where Id = 1;
+
+create table tblInventory
+(
+	Id int primary key identity(1, 1),
+	Product nvarchar(50) not null,
+	ItemsInStock int not null
+)
---
+++ SQL Server Database Project/Tables/tblInventory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblInventory]
+(
+	Id int primary key identity(1, 1),
+	Product nvarchar(50) not null,
+	ItemsInStock int not null
+)
