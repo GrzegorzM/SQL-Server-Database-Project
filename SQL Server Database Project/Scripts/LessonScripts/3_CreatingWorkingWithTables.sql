4b2966f7b80ffb5d57310aacd6ee3ec0b0552836
Kesiu <kesiu.pro@gmail.com>
2020-10-29 22:02:03 +0100

    Creating Tables

---
+++ SQL Server Database Project/Scripts/LessonScripts/3_CreatingWorkingWithTables.sql
@@ -0,0 +1,18 @@
+﻿Use [Sample]
+GO
+
+Create Table tblPerson(
+	Id int Primary Key,
+	Name nvarchar(50) NOT NULL,
+	Email nvarchar(50) NOT NULL,
+	GenderId int
+);
+
+Create Table tblGender(
+	Id int NOT NULL Primary Key,
+	Gender nvarchar(50) NOT NULL
+);
+
+--FK_tblPerson_tblGender
+Alter Table tblPerson Add Constraint tblPerson_GenderId_FK
+Foreign Key (GenderId) references tblGender(Id);
