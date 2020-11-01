db4efc059d60d1b69b415297a4dede1467896674
Kesiu <kesiu.pro@gmail.com>
2020-11-01 23:52:21 +0100

    Check Constraint

---
+++ SQL Server Database Project/Scripts/LessonScripts/0_TableCreation.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE tblPerson (
+    ID int Primary Key Identity(1, 1),
+    LastName varchar(255) NOT NULL Unique,
+    [First Name] varchar(255) NOT NULL,
+    Age int Check(Age > 0) Default(69),
+    City varchar(255),
+    GenderId int Foreign Key References tblGender(Id),
+    Check([First Name] != LastName),
+    CONSTRAINT CK_tblPerson_Age_City CHECK (Age>=18 AND City='London')
+); 
+
+Create Table tblGender(
+	Id int NOT NULL Primary Key,
+	Gender nvarchar(50) NOT NULL
+);
---
+++ SQL Server Database Project/Scripts/LessonScripts/6_CheckConstraint.sql
@@ -0,0 +1,19 @@
+﻿-- Select the table name in ManagementStudio and press Alt+F1 to display table information such as column data types etc.
+Alter Table tblPerson
+Add Age int;
+
+Insert into tblPerson values(4, 'Sara', 'Sara@mail.com', 2, -69);
+Select * from tblPerson;
+Update tblPerson set Age = 69 where Age < 1;
+
+Alter Table tblPerson
+Add Constraint CK_tblPerson_Age Check (Age > 0 AND Age < 150); -- Add Constraint
+Alter Table tblPerson
+NoCheck Constraint CK_tblPerson_Age; -- Disable Constraint
+ALTER TABLE tblPerson NOCHECK CONSTRAINT ALL; -- Disable All Constraint of the specified table(Check, Default etc.)
+
+Insert into tblPerson values(4, 'Sara', 'Sara@mail.com', 2, -69); -- The check constraint does not allow adding a record.
+Insert into tblPerson values(4, 'Sara', 'Sara@mail.com', 2, NULL); -- The check constraint allows to add a record.
+
+Alter Table tblPerson
+Drop Constraint CK_tblPerson_Age; -- Removing Constraint
