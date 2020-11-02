857201ab46293a450bedb68a899c896aec2fe054
Kesiu <kesiu.pro@gmail.com>
2020-11-02 01:08:41 +0100

    Identity Column

---
+++ SQL Server Database Project/Scripts/LessonScripts/7_IdentityColumn.sql
@@ -0,0 +1,13 @@
+﻿Create Table tblPerson1(
+	Id int Identity,
+	Name nvarchar(50) NOT NULL
+)
+
+Insert into tblPerson1 values('Mike');
+Set Identity_Insert tblPerson1 ON;
+Insert into tblPerson1 (Id, Name) values(2, 'Mary');
+Set Identity_Insert tblPerson1 OFF;
+Insert into tblPerson1 values('Mark');
+select * from tblPerson1;
+
+DBCC CHECKIDENT(tblPerson1, ReSeed, 0);-- Reset Identity Value
