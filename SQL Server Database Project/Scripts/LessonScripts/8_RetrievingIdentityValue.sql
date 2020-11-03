4648f3909f26f8a55410b483bb433c17d4cba060
Kesiu <kesiu.pro@gmail.com>
2020-11-03 06:39:53 +0100

    Retrieving Identity Value

---
+++ SQL Server Database Project/Scripts/LessonScripts/8_RetrievingIdentityValue.sql
@@ -0,0 +1,19 @@
+﻿Create Table Test1(
+	Id int Identity(1, 1),
+	Value nvarchar(20)
+);
+
+Create Table Test2(
+	Id int Identity(1 ,1),
+	Value nvarchar(20)
+);
+
+Insert into Test1 Values('ABC');
+Select SCOPE_IDENTITY(); -- Last Identity value in same scope and session.
+Select @@IDENTITY; -- Last Identity value in same session and across any scope.
+Select IDENT_CURRENT('Test2'); -- Last Identity value for specific table across any session and across any scope.
+
+Create Trigger trForInsert on Test1 for Insert
+As Begin
+	Insert into Test2 Values('XYZ');
+End
