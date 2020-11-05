1c502e1ee53a442fd81e4d65b7a07b7b83a347b0
Kesiu <kesiu.pro@gmail.com>
2020-11-05 05:03:10 +0100

    Unique Constraint

---
+++ SQL Server Database Project/Scripts/LessonScripts/9_UniqueConstraint.sql
@@ -0,0 +1,8 @@
+﻿Alter Table tblPerson
+Add Constraint UQ_tblPerson_Email Unique (Email);
+
+insert into tblPerson values(10, 'Catrina', 'default@email.com', 1 , 20);
+insert into tblPerson values(11, 'Anna', 'default@email.com', 1 , 20);
+
+Alter Table tblPerson
+Drop Constraint UQ_tblPerson_Email;
