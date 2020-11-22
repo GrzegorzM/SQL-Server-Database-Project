d277ed40e63cda82594c4be75541bf6e7a920dc6
Kesiu <kesiu.pro@gmail.com>
2020-11-22 03:27:16 +0100

    DateTime Functions

---
+++ SQL Server Database Project/Tables/tblDateTime.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblDateTime]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ColumnTime] TIME NULL, 
+    [ColumnDate] DATE NULL, 
+    [ColumnSmallDatetime] SMALLDATETIME NULL, 
+    [ColumnDatetime] DATETIME NULL, 
+    [ColumnDatetime2] DATETIME2 NULL, 
+    [ColumnDatetimeOffset] DATETIMEOFFSET NULL
+)
