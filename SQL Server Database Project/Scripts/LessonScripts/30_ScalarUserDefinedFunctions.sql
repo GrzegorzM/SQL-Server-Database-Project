926beec966e0afcd01118989a9bc7ca045921f62
Kesiu <kesiu.pro@gmail.com>
2020-11-29 20:46:32 +0100

    Scalar User Defined Function

---
+++ SQL Server Database Project/Scripts/LessonScripts/30_ScalarUserDefinedFunctions.sql
@@ -0,0 +1,15 @@
+﻿Create Function fnCalculateAge(@DateOfBirth DATE) RETURNS INT
+AS BEGIN
+	DECLARE @Age INT;
+
+	SET @Age = DATEDIFF(YEAR, @DateOfBirth, GETDATE()) - 
+			   CASE
+				WHEN (MONTH(@DateOfBirth) > MONTH(GETDATE())) OR 
+					 (MONTH(@DateOfBirth) = MONTH(GETDATE()) AND DAY(@DateOfBirth) > DAY(GETDATE())) THEN 1
+				ELSE 0
+			   END;
+	RETURN @Age;
+END
+
+Select Name, DateOfBirth, dbo.fnCalculateAge(DateOfBirth) as Age from tblEmployee;
+Select Name, DateOfBirth, dbo.fnCalculateAge(DateOfBirth) as Age from tblEmployee where dbo.fnCalculateAge(DateOfBirth) > 30;
