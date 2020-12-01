aec060274ffb22f8cd8ae2027b4a375d65e70ddc
Kesiu <kesiu.pro@gmail.com>
2020-12-01 23:07:36 +0100

    Multi Statement Table Value Function

---
+++ SQL Server Database Project/Scripts/LessonScripts/32_MultiStatementTableValuedFunctions.sql
@@ -0,0 +1,25 @@
+﻿-- Inline table valued function
+CREATE FUNCTION fnInlineGetEmployees() RETURNS TABLE
+AS
+	RETURN (Select Id, Name, CAST(DateOfBirth as DATE) as DateOfBirth from tblEmployee);
+
+-- Multi statement table valued function
+CREATE FUNCTION fnMultiGetEmployees() RETURNS @Table TABLE (Id int, Name nvarchar(20), DateOfBirth date)
+AS BEGIN
+	Insert into @Table
+	Select Id, Name, CAST(DateOfBirth as DATE) from tblEmployee;
+	Return;
+END;
+
+Select * from fnInlineGetEmployees();
+Select * from fnMultiGetEmployees();
+
+-- Correct
+UPDATE fnInlineGetEmployees()
+SET Name = 'Veronica' where Id = 11;
+
+-- Error - not possible to update the underlying table, using an multi-statement table valued function
+UPDATE fnMultiGetEmployees()
+SET Name = 'Veronica' where Id = 11;
+
+-- Inline Table valued functions are better for performance, than multi-statement table valued functions. If the given task, can be achieved using an inline table valued function, always prefer to use them, over multi-statement table valued functions.
