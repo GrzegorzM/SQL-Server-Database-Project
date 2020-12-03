943d63c59b103e2bf480b084fc9cc433e8e2c780
Kesiu <kesiu.pro@gmail.com>
2020-12-04 00:36:41 +0100

    Indexes

---
+++ SQL Server Database Project/Scripts/LessonScripts/35_Indexes.sql
@@ -0,0 +1,9 @@
+﻿Select * from tblEmployee where Salary > 5000 and Salary < 7000;
+
+-- Nonclustered 
+CREATE INDEX IX_tblEmployee_Salary ON tblEmployee (Salary ASC);
+
+-- Display all indexes attached to specified table
+sp_Helpindex tblEmployee;
+
+DROP INDEX tblEmployee.IX_tblEmployee_Salary;
