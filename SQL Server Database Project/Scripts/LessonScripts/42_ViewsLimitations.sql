764907380b49929b73688a1f33a42c08bdd6e082
Kesiu <kesiu.pro@gmail.com>
2020-12-12 16:32:30 +0100

    Views Limitations

---
+++ SQL Server Database Project/Scripts/LessonScripts/42_ViewsLimitations.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW vEmployeeDetails
+AS
+	Select Id, Name, Salary, Gender from tblEmployee;
+Select * from vEmployeeDetails;
+
+-- Not possible to pass parameters to the view
+CREATE VIEW vEmployeeDetails
+@Gender nvarchar(10)
+AS
+	Select Id, Name, Salary, Gender from tblEmployee
+	Where Gender = @Gender;
+-- instead
+Select * from vEmployeeDetails Where Gender = 'Male';
+-- or create parametrized inline table function
+CREATE FUNCTION fnEmployeeDetails(@Gender nvarchar(10))
+RETURNS Table
+AS
+	RETURN (Select Id, Name, Salary, Gender from tblEmployee
+				  Where Gender = @Gender);
+Select * from dbo.fnEmployeeDetails('Male');
+
+-- Not possible to use order by clause
+ALTER VIEW vEmployeeDetails
+AS
+	Select Id, Name, Salary, Gender from tblEmployee
+	Order by Name;
+
+-- Cannot create views on Temp Tables
+CREATE TABLE ##TempTable (ID int, Name nvarchar(20), Gender nvarchar(10));
+CREATE VIEW vOnTempTable -- Error - views or functions are not allowed on temporary tables.
+AS
+	Select * from ##TempTable;
