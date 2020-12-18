7308343f347eaf8facfd2dd415f1d9e4a4b7c6f6
Kesiu <kesiu.pro@gmail.com>
2020-12-18 23:56:36 +0100

    DML Triggers Instead Of Delete

---
+++ SQL Server Database Project/Scripts/LessonScripts/47_TriggersDML_InsteadOfDelete.sql
@@ -0,0 +1,12 @@
+﻿Select * from vEmployeeDetails;
+Delete from vEmployeeDetails where Id = 1; -- Error - View or function 'vEmployeeDetails' is not updatable because the modification affects multiple base tables.
+
+Create Trigger TR_vEmployeeDetails_InsteadOfDelete
+ON vEmployeeDetails INSTEAD OF DELETE
+AS BEGIN
+ Delete tblEmployee from tblEmployee join deleted on tblEmployee.Id = deleted.Id;
+ 
+ -- Another way to do it with subquery
+ -- Delete from tblEmployee where Id in (Select Id from deleted);
+END;
+Delete from vEmployeeDetails where Id in(18, 19);
