01798af17051ff53b9661711388eac729b285576
Kesiu <kesiu.pro@gmail.com>
2020-12-21 23:46:00 +0100

    CTE Update Base Tables

---
+++ SQL Server Database Project/Scripts/LessonScripts/50_UpdatableCTE.sql
@@ -0,0 +1,34 @@
+﻿-- CTE with one base table - Update possible
+WITH EmployeeNameGenderCTE
+AS (
+	SELECT Id, Name, Gender from tblEmployee
+)
+Select * from EmployeeNameGenderCTE;
+
+WITH EmployeeNameGenderCTE
+AS (
+	SELECT Id, Name, Gender from tblEmployee
+)
+UPDATE EmployeeNameGenderCTE SET Gender = 'Female' Where Id = 17;
+
+-- CTE with multiple base tables - Update inpossible when affects multiple tables
+WITH EmployeesByDepartment
+AS (
+	Select Id, Name, Gender, DeptName from tblEmployee e
+	join tblDepartment d on e.DeptID = d.DeptID
+)
+Select * from EmployeesByDepartment;
+
+WITH EmployeesByDepartment
+AS (
+	Select Id, Name, Gender, DeptName from tblEmployee e
+	join tblDepartment d on e.DeptID = d.DeptID
+)
+UPDATE EmployeesByDepartment SET Gender = 'Female' Where Id = 17; -- Update allowed, affects only one base table
+
+WITH EmployeesByDepartment
+AS (
+	Select Id, Name, Gender, DeptName from tblEmployee e
+	join tblDepartment d on e.DeptID = d.DeptID
+)
+UPDATE EmployeesByDepartment SET Gender = 'Female', DeptName = 'HR' Where Id = 17; -- Update disallowed, affects multiple base tables
