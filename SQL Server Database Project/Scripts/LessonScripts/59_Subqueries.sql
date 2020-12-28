724fe1350e4364b76f2e9f78f6b479f9c7a6b1fd
Kesiu <kesiu.pro@gmail.com>
2020-12-28 03:58:25 +0100

    Subqueries

--- SQL Server Database Project/Scripts/LessonScripts/59_Subqueries.sql
+++ SQL Server Database Project/Scripts/LessonScripts/59_Subqueries.sql
@@ -4,3 +4,6 @@ select Name, (select Sum(QuantitySold) from tblProductSales1 where ProductId = t
 select Name, Sum(s.QuantitySold) as TotalSold from tblProducts1 p left join tblProductSales1 s on p.Id = s.ProductId group by Name;
 select Name, (select Sum(QuantitySold * UnitPrice) from tblProductSales1 where ProductId = tblProducts1.Id) as TotalMoneySpend from tblProducts1;
 select Name, Sum(s.QuantitySold * UnitPrice) as TotalMoneySpend from tblProducts1 p left join tblProductSales1 s on p.Id = s.ProductId group by Name;
+
+-- In most cases JOINs are faster than SUB-QUERIEs. However, in cases, where you only need a subset of records from a table that you are joining with, sub-queries can be faster.
+-- For example, if the table to be joined has millions of records and many columns, then the subquery with the filtered data will be faster in terms of performance.
