d65ddce33463715ef9c6152ab006536383ac267f
Kesiu <kesiu.pro@gmail.com>
2020-12-30 00:18:12 +0100

    Except Operator

--- SQL Server Database Project/Scripts/LessonScripts/87_Except.sql
+++ SQL Server Database Project/Scripts/LessonScripts/87_Except.sql
@@ -2,38 +2,38 @@
 SELECT * FROM tblPersonB;
 
 -- Except operator on multiple tables.
-SELECT * FROM tblPersonA -- Not working as intended different Id for same records dont allow to remove records presented in table B.
+SELECT * FROM tblPersonA -- Does not work as intended. A different Id for the same records does not allow to delete the records shown in table B.
 EXCEPT
 SELECT * FROM tblPersonB;
 
-SELECT Name, Gender FROM tblPersonA -- Excluding Id column from the list enable to remove records presented in table B.
+SELECT Name, Gender FROM tblPersonA -- Remove the Id column from the list to get rows shown in table A and exclude the records from table B.
 EXCEPT
 SELECT Name, Gender FROM tblPersonB;
 
-SELECT Gender, Name FROM tblPersonA -- Not working as intended wrong order of columns.
+SELECT Gender, Name FROM tblPersonA -- Does not work as intended wrong order of columns.
 EXCEPT
 SELECT Name, Gender FROM tblPersonB;
 
-SELECT Gender FROM tblPersonA -- Incorrect number of columns must be the same.
+SELECT Gender FROM tblPersonA -- Error - Invalid number of columns - must have an equal number.
 EXCEPT
 SELECT Name, Gender FROM tblPersonB;
 
-SELECT Name, Gender FROM tblPersonB -- Reverse tables to get rows presented in table B and exclude rows from table A.
+SELECT Name, Gender FROM tblPersonB -- Reverse the tables to get the rows shown in table B and exclude the rows from table A.
 EXCEPT
 SELECT Name, Gender FROM tblPersonA;
 
--- Except operator on single table.
+-- Except operator on a single table.
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 4000
 EXCEPT
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 6000;
 
--- Except operator on single table with order by. Order by after secound query.
+-- Except operator on single a table with order by clause. Order by after second query.
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 4000
-ORDER BY Name -- Error - Order by only after the second query.
+ORDER BY Name -- Error - order by only after second query.
 EXCEPT
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 6000;
 
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 4000
 EXCEPT
 SELECT Name, Gender, Salary FROM tblEmployee WHERE Salary >= 6000
-ORDER BY Name;
+ORDER BY Name DESC;
