edef1383119c8cc82883375c4921cdf9d69297fd
Kesiu <kesiu.pro@gmail.com>
2021-01-07 16:59:58 +0100

    Triggers DLL Logon

---
+++ SQL Server Database Project/Scripts/LessonScripts/96_TriggersDDL_Logon.sql
@@ -0,0 +1,21 @@
+﻿SELECT * FROM sys.dm_exec_sessions; -- View
+SELECT is_user_process, original_login_name FROM sys.dm_exec_sessions
+ORDER BY login_time DESC;
+
+CREATE TRIGGER trAuditLogin
+ON ALL SERVER FOR LOGON
+AS BEGIN
+	DECLARE @LoginName Nvarchar(100);
+	SET @LoginName = ORIGINAL_LOGIN();
+
+	IF (
+		SELECT COUNT(session_id) FROM sys.dm_exec_sessions
+		WHERE is_user_process = 1 AND original_login_name = @LoginName
+		) > 3
+	BEGIN
+		PRINT('Fourth connection attempt by ' + @LoginName + ' blocked.');
+		ROLLBACK;	
+	END;
+END;
+
+EXECUTE sp_readerrorlog;
