9aa65775b0728e3bd6dd8f07044f27e9d1959b23
Kesiu <kesiu.pro@gmail.com>
2021-01-12 23:48:48 +0100

    Rollup

---
+++ SQL Server Database Project/Scripts/LessonScripts/102_Rollup.sql
@@ -0,0 +1,46 @@
+﻿SELECT Country, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY ROLLUP(Country);
+
+-- The above query can also be rewritten as shown below
+SELECT Country, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY Country WITH ROLLUP;
+
+-- We can also use UNION ALL operator along with GROUP BY
+SELECT Country, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY Country
+UNION ALL
+SELECT NULL, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries;
+
+-- We can also use Grouping Sets to achieve the same result
+SELECT Country, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY GROUPING SETS
+(
+    (Country),
+    ()
+);
+
+-- Using ROLLUP with GROUP BY
+SELECT Country, Gender, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY ROLLUP(Country, Gender);
+-- OR
+SELECT Country, Gender, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY Country, Gender WITH ROLLUP;
+
+-- Using UNION ALL with GROUP BY
+SELECT Country, Gender, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY Country, Gender
+UNION ALL
+SELECT Country, NULL, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY Country
+UNION ALL
+SELECT NULL, NULL, SUM(Salary) AS TotalSalary
+FROM tblEmployeeCountries;
+
+-- Using GROUPING SETS
+SELECT Country, Gender, SUM(Salary) AS TotalSalary FROM tblEmployeeCountries
+GROUP BY GROUPING SETS
+(
+    (Country, Gender),
+    (Country),
+    ()
+);
