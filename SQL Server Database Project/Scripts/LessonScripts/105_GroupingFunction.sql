26becd5370c856642981c7b0e33cdc8c310d47f8
Kesiu <kesiu.pro@gmail.com>
2021-01-18 00:05:18 +0100

    Grouping Function

--- SQL Server Database Project/Scripts/LessonScripts/105_GroupingFunction.sql
+++ SQL Server Database Project/Scripts/LessonScripts/105_GroupingFunction.sql
@@ -3,7 +3,7 @@
 SELECT Continent, Country, City, SUM(SaleAmount) as TotalSales FROM tblCountrySales
 GROUP BY Continent, Country, City;
 
--- Does not work without a rollup.
+-- Does not work without a rollup, cube or grouping sets.
 SELECT Continent, Country, City, SUM(SaleAmount) as TotalSales,
 	GROUPING(Continent) as Grouping_Continent,
 	GROUPING(Country) as Grouping_Country,
@@ -31,9 +31,31 @@ SELECT
 GROUP BY ROLLUP(Continent, Country, City);
 
 Update tblCountrySales Set City = NULL where Id = 1
-SELECT -- It will not work as expected if the table contains null data. Replacing 'null' data to 'all' which is misleading.
+SELECT -- It will not work as expected if the table contains null data. Replacing 'null' to 'all' which is misleading.
 	ISNULL(Continent, 'ALL') AS Continent,
 	ISNULL(Country, 'ALL') AS Country,
 	ISNULL(City, 'ALL') AS City,
 	SUM(SaleAmount) as TotalSales FROM tblCountrySales
 GROUP BY ROLLUP(Continent, Country, City);
+
+-- Cube example
+SELECT 
+	CASE WHEN GROUPING(Continent) = 1 THEN 'ALL' ELSE ISNULL(Continent, 'Unknown') END AS Continent,
+	CASE WHEN GROUPING(Country) = 1 THEN 'ALL' ELSE ISNULL(Country, 'Unknown') END AS Country,
+	CASE WHEN GROUPING(City) = 1 THEN 'ALL' ELSE ISNULL(City, 'Unknown') END AS City,
+	SUM(SaleAmount) as TotalSales FROM tblCountrySales
+GROUP BY CUBE(Continent, Country, City);
+
+-- Grouping Sets example
+SELECT 
+	CASE WHEN GROUPING(Continent) = 1 THEN 'ALL' ELSE ISNULL(Continent, 'Unknown') END AS Continent,
+	CASE WHEN GROUPING(Country) = 1 THEN 'ALL' ELSE ISNULL(Country, 'Unknown') END AS Country,
+	CASE WHEN GROUPING(City) = 1 THEN 'ALL' ELSE ISNULL(City, 'Unknown') END AS City,
+	SUM(SaleAmount) as TotalSales FROM tblCountrySales
+GROUP BY GROUPING SETS
+(	
+	(Continent, Country, City),
+	(Continent, Country),
+	(Continent),
+	()
+);
