f12e3fe7fb067c3f642cdad9b8aa2d87004da540
Kesiu <kesiu.pro@gmail.com>
2021-01-25 22:58:29 +0100

    Calculate Running Total

--- SQL Server Database Project/Scripts/LessonScripts/112_CalculateRunningTotal.sql
+++ SQL Server Database Project/Scripts/LessonScripts/112_CalculateRunningTotal.sql
@@ -3,4 +3,4 @@
 SELECT Name, Gender, Salary, SUM(Salary) OVER (PARTITION BY Gender ORDER BY ID) AS RunningTotal FROM tblEmployee;
 
 -- Using a column with duplicate values(Gender) in the ORDER BY clause causes all duplicate values to be added to the running total at once.
-SELECT Name, Gender, Salary, SUM(Salary) OVER (ORDER BY Gender) AS RunningTotal FROM tblEmployee; -- SQL Server 2019, this syntax returns the salary sum for an entire partition, and then adds the sum of the next partition to that number.
+SELECT Name, Gender, Salary, SUM(Salary) OVER (ORDER BY Gender) AS RunningTotal FROM tblEmployee;
