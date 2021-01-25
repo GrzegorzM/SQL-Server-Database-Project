7159bb83645efecdaa5e179b1a383324fabd1c32
Kesiu <kesiu.pro@gmail.com>
2021-01-25 23:26:27 +0100

    NTILE Function

---
+++ SQL Server Database Project/Scripts/LessonScripts/113_Ntile.sql
@@ -0,0 +1,5 @@
+﻿SELECT Name, Gender, Salary, NTILE(2) OVER (ORDER BY Salary) AS [NTILE] FROM tblEmployee;
+SELECT Name, Gender, Salary, NTILE(4) OVER (ORDER BY Salary) AS [NTILE] FROM tblEmployee;
+
+SELECT Name, Gender, Salary, NTILE(2) OVER (PARTITION BY Gender ORDER BY Salary) AS [NTILE] FROM tblEmployee;
+SELECT Name, Gender, Salary, NTILE(4) OVER (PARTITION BY Gender ORDER BY Salary) AS [NTILE] FROM tblEmployee;
