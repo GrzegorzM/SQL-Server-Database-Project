fb59b28739809f31626bddb348f6c25cc06226c6
Kesiu <kesiu.pro@gmail.com>
2021-01-26 21:09:52 +0100

    Lead and Lag Functions

---
+++ SQL Server Database Project/Scripts/LessonScripts/114_LeadLag.sql
@@ -0,0 +1,9 @@
+﻿SELECT Name, Gender, Salary, LEAD(Salary) OVER (ORDER BY Salary) AS [LEAD] FROM tblEmployee;
+SELECT Name, Gender, Salary, LEAD(Salary, 2) OVER (ORDER BY Salary) AS [LEAD] FROM tblEmployee;
+SELECT Name, Gender, Salary, LEAD(Salary, 2, -1) OVER (ORDER BY Salary) AS [LEAD] FROM tblEmployee;
+SELECT Name, Gender, Salary, LEAD(Salary, 2, -1) OVER (PARTITION BY Gender ORDER BY Salary) AS [LEAD] FROM tblEmployee;
+
+SELECT Name, Gender, Salary, LAG(Salary) OVER (ORDER BY Salary) AS [LAG] FROM tblEmployee;
+SELECT Name, Gender, Salary, LAG(Salary, 2) OVER (ORDER BY Salary) AS [LAG] FROM tblEmployee;
+SELECT Name, Gender, Salary, LAG(Salary, 2, -1) OVER (ORDER BY Salary) AS [LAG] FROM tblEmployee;
+SELECT Name, Gender, Salary, LAG(Salary, 2, -1) OVER (PARTITION BY Gender ORDER BY Salary) AS [LAG] FROM tblEmployee;
