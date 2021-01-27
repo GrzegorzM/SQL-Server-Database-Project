ec19e5939bf56f8cf643a7eab18d572620d09431
Kesiu <kesiu.pro@gmail.com>
2021-01-27 22:04:48 +0100

    First_Value Function

---
+++ SQL Server Database Project/Scripts/LessonScripts/115_FirstValue.sql
@@ -0,0 +1,3 @@
+﻿SELECT Name, Gender, Salary, FIRST_VALUE(Name) OVER (ORDER BY Salary) AS [FIRST_VALUE] FROM tblEmployee;
+SELECT Name, Gender, Salary, FIRST_VALUE(Name) OVER (PARTITION BY Gender ORDER BY Salary) AS [FIRST_VALUE] FROM tblEmployee;
+-- FIRST_VALUE function - Retrieves the first value from the specified column, ORDER BY clause is required.
