01451bcbc0d76ed795a2338f59aab119df50b23d
Kesiu <kesiu.pro@gmail.com>
2021-01-30 23:06:50 +0100

    IIF Function

---
+++ SQL Server Database Project/Scripts/LessonScripts/122_IIF.sql
@@ -0,0 +1,14 @@
+﻿DECLARE @Gender INT;
+SET @Gender = 1;
+SELECT IIF( @Gender = 1, 'Male', 'Female') AS Gender;
+
+-- Using CASE statement
+SELECT Name, Salary, 
+    CASE WHEN Salary >= 4500
+      THEN 'High Salary'
+      ELSE 'Low Salary'
+    END AS SalaryDescription
+FROM tblEmployee;
+
+-- Using IIF function
+SELECT Name, Salary, IIF(Salary >= 4500, 'High Salary', 'Low Salary') AS SalaryDescription FROM tblEmployee;
