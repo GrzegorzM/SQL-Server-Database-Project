f2ff763f30e31fe5d0d7dc0074cf8e5023d68136
Kesiu <kesiu.pro@gmail.com>
2021-01-31 23:42:50 +0100

    Rows and Range differences

---
+++ SQL Server Database Project/Scripts/LessonScripts/117_RowsRangeDifferences.sql
@@ -0,0 +1,11 @@
+﻿-- All statements below produce same results if there is not duplicates.
+SELECT Name, Salary, SUM(Salary) OVER (ORDER BY Salary) AS RunningTotal FROM tblEmployee;
+SELECT Name, Salary, SUM(Salary) OVER (ORDER BY Salary RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS RunningTotal FROM tblEmployee;
+SELECT Name, Salary, SUM(Salary) OVER (ORDER BY Salary ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS RunningTotal FROM tblEmployee;
+
+-- The main difference between ROWS and RANGE is in the way duplicate rows are treated. ROWS treat duplicates as distinct values, where as RANGE treats them as a single entity.
+SELECT Name, Salary, 
+	SUM(Salary) OVER (ORDER BY Salary) AS [Default],
+	SUM(Salary) OVER (ORDER BY Salary RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS [RANGE],
+	SUM(Salary) OVER (ORDER BY Salary ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS [ROWS]
+FROM tblEmployee;
