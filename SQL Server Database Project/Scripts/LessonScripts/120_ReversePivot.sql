98952e711d5c7cca667d4bce74eca159fa28d623
Kesiu <kesiu.pro@gmail.com>
2021-02-03 00:33:17 +0100

    Reversing Pivot with Unpivot

---
+++ SQL Server Database Project/Scripts/LessonScripts/120_ReversePivot.sql
@@ -0,0 +1,28 @@
+﻿SELECT * FROM tblProductSales;
+
+SELECT SalesAgent, UK, Germany FROM tblProductSales
+PIVOT(
+	SUM(SalesAmount) FOR SalesCountry IN(UK, Germany)
+) AS PivotExample;
+
+-- It is possible to UNPIVOT data that has an Id column because the Pivot operator does not aggregate it.
+SELECT SalesAgent, SalesCountry, SalesAmount FROM
+(SELECT SalesAgent, UK, USA, Germany FROM tblProductSales
+PIVOT(
+	SUM(SalesAmount) FOR SalesCountry IN(UK, USA, Germany)
+) AS PivotExample) AS PivotOperator
+UNPIVOT(
+	SalesAmount FOR SalesCountry IN(UK, USA, Germany)
+) AS UnpivotExample;
+SELECT * FROM tblProductSales;
+
+-- If UNPIVOT operator is used with the query below, it is impossible to get orginial data back as the PIVOT operator has already aggregated the data, and there is no way for SQL Server to know how to undo the aggregations.
+SELECT SalesAgent, SalesCountry, SalesAmount FROM
+(SELECT SalesAgent, UK, USA, Germany FROM tblProductSalesNoIdColumn
+PIVOT(
+	SUM(SalesAmount) FOR SalesCountry IN(UK, USA, Germany)
+) AS PivotExample) AS PivotOperator
+UNPIVOT(
+	SalesAmount FOR SalesCountry IN(UK, USA, Germany)
+) AS UnpivotExample;
+SELECT * FROM tblProductSalesNoIdColumn;
