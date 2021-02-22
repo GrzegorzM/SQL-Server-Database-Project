22d7537db3b9e66d197b13c59df987df6d9d0444
Kesiu <kesiu.pro@gmail.com>
2021-02-22 23:43:23 +0100

    Dynamic SQL

---
+++ SQL Server Database Project/Scripts/LessonScripts/138_DynamicSQL.sql
@@ -0,0 +1,12 @@
+﻿DECLARE @sql NVARCHAR(1000);
+SET @sql = 'SELECT * FROM tblEmployee';
+EXECUTE SP_EXECUTESQL @sql;
+
+DECLARE @sql NVARCHAR(1000);
+DECLARE @params NVARCHAR(1000);
+SET @sql = 'SELECT * FROM tblEmployee' + ' Where Name=@Name AND Gender=@Gender';
+SET @params = '@Name NVARCHAR(100), @Gender NVARCHAR(10)';
+PRINT @sql;
+--EXECUTE SP_EXECUTESQL @sql, @params, @Name = 'John', @Gender = 'Male';
+--EXECUTE SP_EXECUTESQL @sql, @params, @Gender = 'Male', @Name = 'John';
+EXECUTE SP_EXECUTESQL @sql, @params, 'John', 'Male';
