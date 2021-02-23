197a41e6bca8f1c24091addb38a5d5aa405d3c53
Kesiu <kesiu.pro@gmail.com>
2021-02-23 23:52:53 +0100

    Transaction - Non repeatable read

---
+++ SQL Server Database Project/Scripts/LessonScripts/73_Transaction_NonRepeatableRead.sql
@@ -0,0 +1,21 @@
+﻿SELECT * FROM tblProduct;
+
+-- Transaction 1
+BEGIN TRANSACTION
+	SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Old Value
+	WAITFOR DELAY '00:00:10';
+	SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- New Value(74)
+COMMIT TRANSACTION
+-- Transaction 2
+UPDATE tblProduct SET QuantityAvailable = 74 WHERE ProductId = 1;
+
+
+-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+BEGIN TRANSACTION
+	SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Old Value
+	WAITFOR DELAY '00:00:10';
+	SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Old Value
+COMMIT TRANSACTION
+-- Transaction 2
+UPDATE tblProduct SET QuantityAvailable = 74 WHERE ProductId = 1; -- Update only after completing the transaction. 
