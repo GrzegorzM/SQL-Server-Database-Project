21ae754eb210bf7db99164d59ee4910fd231ae5c
Kesiu <kesiu.pro@gmail.com>
2021-02-25 23:58:38 +0100

    Snapshot Isolation Level

---
+++ SQL Server Database Project/Scripts/LessonScripts/75_Transaction_Snapshot.sql
@@ -0,0 +1,35 @@
+﻿-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 75 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRANSACTION;
+SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Blocked.
+COMMIT TRANSACTION;
+
+
+ALTER DATABASE Sample SET ALLOW_SNAPSHOT_ISOLATION ON;
+-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 74 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+BEGIN TRANSACTION;
+SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Returns the "old" value from a temporary table.
+COMMIT TRANSACTION;
+
+
+-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 75 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 76 WHERE ProductId = 1; -- Blocked when transaction 1 is uncommitted and returns an error after committing the first transaction. Cannot override update(lost update) while another transaction is updated.
+COMMIT TRANSACTION;
