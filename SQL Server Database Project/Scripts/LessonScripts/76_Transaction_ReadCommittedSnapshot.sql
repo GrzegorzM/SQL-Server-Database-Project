460af781e6da41752f38d67257b32cf1daa679e7
Kesiu <kesiu.pro@gmail.com>
2021-02-27 22:53:44 +0100

    Transaction Read Commited Snapshot

---
+++ SQL Server Database Project/Scripts/LessonScripts/76_Transaction_ReadCommittedSnapshot.sql
@@ -0,0 +1,37 @@
+﻿-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 75 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Blocked till commit.
+COMMIT TRANSACTION;
+
+
+-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 75 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+ALTER DATABASE Sample SET READ_COMMITTED_SNAPSHOT ON; -- Blocked till all connections(windows in Management Studio) are closed.
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Not blocked, returns the last committed  data - old data.
+COMMIT TRANSACTION;
+
+-- Same results as above but using a snapshot.
+ALTER DATABASE Sample SET READ_COMMITTED_SNAPSHOT OFF; -- Blocked till all connections(windows in Management Studio) are closed.
+-- Transaction 1
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+BEGIN TRANSACTION;
+UPDATE tblProduct SET QuantityAvailable = 75 WHERE ProductId = 1;
+COMMIT TRANSACTION;
+-- Transaction 2
+ALTER DATABASE Sample SET ALLOW_SNAPSHOT_ISOLATION ON; -- Blocked till all connections(windows in Management Studio) are closed.
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+BEGIN TRANSACTION;
+SELECT QuantityAvailable FROM tblProduct WHERE ProductId = 1; -- Not blocked, returns the last committed  data - old data.
+COMMIT TRANSACTION;
