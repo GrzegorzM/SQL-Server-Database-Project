ebf9708231b242d845ae60a4730855e5c310cf36
Kesiu <kesiu.pro@gmail.com>
2021-03-01 23:41:41 +0100

    Transaction Deadlock

---
+++ SQL Server Database Project/Scripts/LessonScripts/78_Transaction_Deadlock.sql
@@ -0,0 +1,14 @@
+﻿-- Transaction 1
+BEGIN TRAN
+UPDATE tblPersonA SET Gender = 'Female' WHERE Id = 1;
+UPDATE tblPersonB SET Gender = 'Female' WHERE Id = 1; -- Blocked until database engine solved deadlock in transaction 2.
+COMMIT TRANSACTION
+
+-- Transaction 2
+BEGIN TRAN
+UPDATE tblPersonB SET Gender = 'Male' WHERE Id = 1;
+UPDATE tblPersonA SET Gender = 'Male' WHERE Id = 1; -- ERROR - Transaction (Process ID 58) was deadlocked on lock resources with another process and has been chosen as the deadlock victim. Rerun the transaction.
+COMMIT TRANSACTION
+
+Select * FROM tblPersonA WHERE Id = 1; -- 'Female'
+Select * FROM tblPersonB WHERE Id = 1; -- 'Female'
